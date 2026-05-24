2f1d66175d3a0be5605624763f4f19b11bf45264
Chris Tattum <chris.tattum@gmail.com>
2026-05-24 10:28:08 +0100

    Add more tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -7,5 +7,13 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_plain(self):
+        node = TextNode("This is plain text", TextType.PLAIN)
+        self.assertEqual(repr(node), f"TextNode(This is plain text, TextType.PLAIN, None)")
+        
+    def test_bold(self):
+        node = TextNode("This is bold text", TextType.BOLD)
+        self.assertEqual(repr(node), f"TextNode(This is bold text, TextType.BOLD, None)")
+
 if __name__ == "__main__":
     unittest.main()
