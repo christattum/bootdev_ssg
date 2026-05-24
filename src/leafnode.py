0d3b49fd936e97d140b928cc40ec6a6f7637f995
Chris Tattum <chris.tattum@gmail.com>
2026-05-24 15:04:10 +0100

    More tests

--- src/leafnode.py
+++ src/leafnode.py
@@ -16,7 +16,7 @@ class LeafNode(HTMLNode):
             return self.value
         
         if self.props is None:
-            return f'<{self.tag}>{self.value}<{self.tag}>'
+            return f'<{self.tag}>{self.value}</{self.tag}>'
         
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
 
--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -7,6 +7,10 @@ class TestHTMLNode(unittest.TestCase):
         node2 = LeafNode("a", "Click Me!", {"href": "https://www.google.com"})
         self.assertEqual(node1, node2)
 
-    def test_to_html(self):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
         node = LeafNode("a", "Click Me!", {"href": "https://www.google.com"})
-        self.assertEqual(node.to_html(), f'<a href="https://www.google.com">Click Me!</a>')
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click Me!</a>')
