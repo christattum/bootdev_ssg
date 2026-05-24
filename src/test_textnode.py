23530d1a017fd134b4641af4032fb5d7aada4d63
Chris Tattum <chris.tattum@gmail.com>
2026-05-24 10:32:59 +0100

    More tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -15,5 +15,14 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is bold text", TextType.BOLD)
         self.assertEqual(repr(node), f"TextNode(This is bold text, TextType.BOLD, None)")
 
+    def test_link_with_url(self):
+        node = TextNode("This is a link", TextType.LINK, "http://example.com")
+        self.assertEqual(repr(node), f"TextNode(This is a link, TextType.LINK, http://example.com)")
+
+    def test_url_defaults_to_none(self):
+        node = TextNode("Link without a url", TextType.LINK)
+        self.assertEqual(node.url, None)
+
+
 if __name__ == "__main__":
     unittest.main()
