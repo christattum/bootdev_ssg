f67965cc8ea6a47a2cf715ba1cd71c8c2a447542
Chris Tattum <chris.tattum@gmail.com>
2026-05-24 18:05:41 +0100

    Simplify test

--- src/test_mardown_parser.py
+++ src/test_mardown_parser.py
@@ -7,19 +7,7 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         node = TextNode("This is text with a `code block` word", TextType.PLAIN)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
-        # [
-        #     TextNode("This is text with a ", TextType.TEXT),
-        #     TextNode("code block", TextType.CODE),
-        #     TextNode(" word", TextType.TEXT),
-        # ]   
-
         self.assertEqual(len(new_nodes), 3)
-        self.assertIsInstance(new_nodes[0], TextNode)
-        self.assertIsInstance(new_nodes[1], TextNode)
-        self.assertIsInstance(new_nodes[2], TextNode)
-        self.assertEqual(new_nodes[0].text_type, TextType.PLAIN)
-        self.assertEqual(new_nodes[0].text, "This is text with a ")
-        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
-        self.assertEqual(new_nodes[1].text, "code block")
-        self.assertEqual(new_nodes[2].text_type, TextType.PLAIN)
-        self.assertEqual(new_nodes[2].text, " word")
+        self.assertEqual(new_nodes[0], TextNode("This is text with a ", TextType.PLAIN))
+        self.assertEqual(new_nodes[1], TextNode("code block", TextType.CODE))
+        self.assertEqual(new_nodes[2], TextNode(" word", TextType.PLAIN))
