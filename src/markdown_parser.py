748f8b67f9b2ef56969f730408a0039e38aa29a9
Chris Tattum <chris.tattum@gmail.com>
2026-05-24 18:00:57 +0100

    Test passing

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -3,10 +3,13 @@ from textnode import TextNode, TextType
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
-        split_text = old_node.text.split(delimiter)
-        for text in split_text:
-            new_node = TextNode(text, TextType.PLAIN)
-            new_nodes.append(new_node)
+        # maxsplit = 2, for opening and closing delimiter
+        # which results in 3 strings
+        split_text = old_node.text.split(delimiter, maxsplit=2)
+        if len(split_text) == 3:
+            new_nodes.append(TextNode(split_text[0], TextType.PLAIN))
+            new_nodes.append(TextNode(split_text[1], text_type))
+            new_nodes.append(TextNode(split_text[2], TextType.PLAIN))
 
     return new_nodes
 
