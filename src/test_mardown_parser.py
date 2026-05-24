bc35ad7f41b685df337c45f05c2b806c648befec
Chris Tattum <chris.tattum@gmail.com>
2026-05-24 17:40:39 +0100

    add markdown parser tests

---
+++ src/test_mardown_parser.py
@@ -0,0 +1,25 @@
+import unittest
+from textnode import TextNode, TextType
+from markdown_parser import split_nodes_delimiter
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_single_node(self):
+        node = TextNode("This is text with a `code block` word", TextType.PLAIN)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+        # [
+        #     TextNode("This is text with a ", TextType.TEXT),
+        #     TextNode("code block", TextType.CODE),
+        #     TextNode(" word", TextType.TEXT),
+        # ]   
+
+        self.assertEqual(len(new_nodes), 3)
+        self.assertIsInstance(new_nodes[0], TextNode)
+        self.assertIsInstance(new_nodes[1], TextNode)
+        self.assertIsInstance(new_nodes[2], TextNode)
+        self.assertEqual(new_nodes[0].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[0].value, "This is text with a ")
+        self.assertEqual(new_nodes[1].text_type, TextType.CODE)
+        self.assertEqual(new_nodes[1].value, "code block")
+        self.assertEqual(new_nodes[2].text_type, TextType.PLAIN)
+        self.assertEqual(new_nodes[2].value, " word")
