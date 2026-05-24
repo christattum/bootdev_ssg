1e34ddd407e24fe8cd7160e97c4a9f18ad9a3ee4
Chris Tattum <chris.tattum@gmail.com>
2026-05-24 19:13:24 +0100

    Add further inline tests

--- src/test_mardown_parser.py
+++ src/test_mardown_parser.py
@@ -1,9 +1,11 @@
 import unittest
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
+from leafnode import LeafNode
 from markdown_parser import split_nodes_delimiter
 
 class TestSplitNodesDelimiter(unittest.TestCase):
-    def test_single_node(self):
+    def test_plain_text_node(self):
         node = TextNode("This is text with a `code block` word", TextType.PLAIN)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
 
@@ -12,7 +14,28 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         self.assertEqual(new_nodes[1], TextNode("code block", TextType.CODE))
         self.assertEqual(new_nodes[2], TextNode(" word", TextType.PLAIN))
 
+    def test_bold_text_node(self):
+        node = TextNode("This is text with a **bold** word", TextType.PLAIN)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0], TextNode("This is text with a ", TextType.PLAIN))
+        self.assertEqual(new_nodes[1], TextNode("bold", TextType.BOLD))
+        self.assertEqual(new_nodes[2], TextNode(" word", TextType.PLAIN))
+
+    def test_italic_text_node(self):
+        node = TextNode("This is text with an _italic_ word", TextType.PLAIN)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+
+        self.assertEqual(len(new_nodes), 3)
+        self.assertEqual(new_nodes[0], TextNode("This is text with an ", TextType.PLAIN))
+        self.assertEqual(new_nodes[1], TextNode("italic", TextType.ITALIC))
+        self.assertEqual(new_nodes[2], TextNode(" word", TextType.PLAIN))     
+
     def test_single_node_without_closing_delimiter_raises_exception(self):
         node = TextNode("This is text with a `code block without closing delimiter", TextType.PLAIN)
         with self.assertRaises(ValueError):
             split_nodes_delimiter([node], "`", TextType.CODE)
+
+    def test_non_text_node_will_pass_through_as_new_node(self):
+        pass
