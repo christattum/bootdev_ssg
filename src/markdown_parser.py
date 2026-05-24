e28e9e12510e2ceb22c02210cae0c0ef8daacfb3
Chris Tattum <chris.tattum@gmail.com>
2026-05-24 17:48:10 +0100

    Break it down

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -1,2 +1,12 @@
+from textnode import TextNode, TextType
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    return []
+    new_nodes = []
+    for old_node in old_nodes:
+        split_text = old_node.text.split(delimiter)
+        for text in split_text:
+            new_node = TextNode(text, TextType.PLAIN)
+            new_nodes.append(new_node)
+
+    return new_nodes
+
