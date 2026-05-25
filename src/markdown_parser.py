faca0c88270b1d99542983140db95304b880bd11
Chris Tattum <chris.tattum@gmail.com>
2026-05-25 10:13:56 +0100

    Get it working

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -25,7 +25,32 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return new_nodes
 
 def split_nodes_image(old_nodes):
-    return []
+    new_nodes = []
+
+    for old_node in old_nodes:
+        # Only split PLAIN text nodes, otherwise pass through as is
+        if old_node.text_type != TextType.PLAIN:
+            new_nodes.append(old_node)
+            continue
+
+        text = old_node.text
+        images = extract_markdown_images(text)
+        for image in images:
+            image_text = f"![{image[0]}]({image[1]})"
+
+            # Break off first part of string before image, then image and rest of string part
+            split_text = text.split(image_text)
+
+            # Add first part as TextNode
+            new_nodes.append(TextNode(split_text[0], TextType.PLAIN))
+
+            # Then add the image
+            new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
+            
+            # Set text to remaining part, before looping around for next image
+            text = split_text[1]
+
+    return new_nodes
 
 def split_nodes_link(old_nodes):
     return []
