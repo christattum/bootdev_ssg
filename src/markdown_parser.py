c3b36f4b9513592eaa9a15202fcbb1e9b653d474
Chris Tattum <chris.tattum@gmail.com>
2026-05-25 10:21:49 +0100

    Get link working

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -46,14 +46,39 @@ def split_nodes_image(old_nodes):
 
             # Then add the image
             new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
-            
+
             # Set text to remaining part, before looping around for next image
             text = split_text[1]
 
     return new_nodes
 
 def split_nodes_link(old_nodes):
-    return []
+    new_nodes = []
+
+    for old_node in old_nodes:
+        # Only split PLAIN text nodes, otherwise pass through as is
+        if old_node.text_type != TextType.PLAIN:
+            new_nodes.append(old_node)
+            continue
+
+        text = old_node.text
+        links = extract_markdown_links(text)
+        for link in links:
+            link_text = f"[{link[0]}]({link[1]})"
+
+            # Break off first part of string before link, then link and rest of string part
+            split_text = text.split(link_text)
+
+            # Add first part as TextNode
+            new_nodes.append(TextNode(split_text[0], TextType.PLAIN))
+
+            # Then add the link
+            new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
+            
+            # Set text to remaining part, before looping around for next link
+            text = split_text[1]
+
+        return new_nodes
 
 def extract_markdown_images(text):
     matches = re.findall(r"!\[(.+?)\]\((.+?)\)", text)
--- src/test_mardown_parser.py
+++ src/test_mardown_parser.py
@@ -91,3 +91,23 @@ class TestSplitImages(unittest.TestCase):
             ],
             new_nodes,
         )
+
+class TestSplitLinks(unittest.TestCase):
+
+    def test_given_example(self):
+        node = TextNode(
+            "This is text with a [link](https://www.google.com) and another [second link](https://www.apple.com)",
+            TextType.PLAIN,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.PLAIN),
+                TextNode("link", TextType.LINK, "https://www.google.com"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode(
+                    "second link", TextType.LINK, "https://www.apple.com"
+                ),
+            ],
+            new_nodes,
+        )
