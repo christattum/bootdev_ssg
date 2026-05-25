0403b48dc6aa35fba66078a51cd54a5c3a52e149
Chris Tattum <chris.tattum@gmail.com>
2026-05-25 14:55:18 +0100

    Add unhappy path tests

--- src/test_mardown_parser.py
+++ src/test_mardown_parser.py
@@ -328,6 +328,11 @@ class TestBlockTypes(unittest.TestCase):
         block_type = block_to_block_type(block)
         self.assertEqual(block_type, BlockType.HEADING)
 
+    def test_heading_without_space_is_paragraph_block(self):
+        block = "#This is not a heading 1"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
     def test_valid_code_block(self):
         block = """
 ```
@@ -341,11 +346,20 @@ This is second line
     def test_valid_quote_block(self):
         block = """
 > This is quote line 1
-> This is quote line 2
+>This is quote line 2
 > This is quote line 2
 """
         block_type = block_to_block_type(block)
         self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_quote_block_without_all_correct_leading_symbol_is_paragraph(self):
+        block = """
+> This is quote line 1
+> This is quote line 2
+This is quote line 2
+"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
 
     def test_valid_unordered_list_block(self):
         block = """
@@ -355,6 +369,15 @@ This is second line
         block_type = block_to_block_type(block)
         self.assertEqual(block_type, BlockType.UNORDERED_LIST)
 
+    def test_unordered_list_without_space_is_paragraph_block(self):
+        block = """
+- This is first item
+-This is second item
+"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+
     def test_valid_ordered_list_block(self):
         block = """
 1. This is first item
@@ -362,3 +385,29 @@ This is second line
 """
         block_type = block_to_block_type(block)
         self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+
+    def test_ordered_list_without_spaces_is_paragraph(self):
+        block = """
+1. This is first item
+2.This is second item
+"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_ordered_list_without_fullstop_is_paragraph(self):
+        block = """
+1. This is first item
+2 This is second item
+"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+
+    def test_ordered_list_with_out_of_order_numbers_is_paragraph(self):
+        block = """
+2. This is first item
+1. This is second item
+"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
