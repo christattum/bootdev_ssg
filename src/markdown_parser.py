82251e61e9113d9f98d6fc932ce2e92efa9dc790
Chris Tattum <chris.tattum@gmail.com>
2026-05-25 10:38:01 +0100

    Add initial broken version

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -81,7 +81,13 @@ def split_nodes_link(old_nodes):
         return new_nodes
     
 def text_to_textnodes(text):
-    return []
+    initial_node = TextNode(text, TextType.PLAIN)
+    nodes = split_nodes_link([initial_node])
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    return nodes
 
 def extract_markdown_images(text):
     matches = re.findall(r"!\[(.+?)\]\((.+?)\)", text)
