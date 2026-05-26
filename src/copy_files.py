b4a4bc354beffb922cee2041858e2403bfa2afd6
Chris Tattum <chris.tattum@gmail.com>
2026-05-26 21:49:24 +0100

    Get copy and delete working

--- src/copy_files.py
+++ src/copy_files.py
@@ -1,9 +1,12 @@
 import os
+import shutil
 
 def delete_files(target_dir: str, logger, test_mode: bool = True):
 
     print()
     print(f"Deleting files from {target_dir}")
+    if test_mode:
+        logger("Running in test mode")
 
     # cwd should be the workspace root, or from where main.sh is run
     cwd = os.getcwd()
@@ -13,10 +16,16 @@ def delete_files(target_dir: str, logger, test_mode: bool = True):
     print("Target Path: ", target_path)
 
     logger(f"Removing files from {target_path}")
+    if not test_mode:
+        shutil.rmtree(target_path)
 
 def copy_files_r(source_path: str, target_path: str, logger, test_mode: bool = True):
     files = os.listdir(source_path)
     print(f"Files in {source_path}", files)
+
+    # Create target directory  
+    if not test_mode and not os.path.exists(target_path):
+        os.mkdir(target_path)
 
     for file in files:
         full_source_path_name = os.path.join(source_path, file)
@@ -27,6 +36,8 @@ def copy_files_r(source_path: str, target_path: str, logger, test_mode: bool = T
         elif os.path.isfile(full_source_path_name):
             # Copy file
             logger(f"Copying {full_source_path_name} to {full_target_path_name}")
+            if not test_mode:
+                shutil.copy(full_source_path_name, full_target_path_name)
         else:
             # May be a simlink or something else
             raise RuntimeError('Not sure what to do here!')
--- src/test_copy_files.py
+++ src/test_copy_files.py
@@ -10,16 +10,19 @@ class TestCopyFiles(unittest.TestCase):
         self.log.append(text)
         print(text)
 
+    @unittest.skip("need to control which runs")
     def test_copy_files(self):
-        copy_files("./static", "./public", self.logger)
+        copy_files("./static", "./public", self.logger, True)
         self.assertListEqual([
             "Running in test mode",
             "Copying /Users/chris/Workspace/Personal/boot.dev/bootdev_ssg/static/images/tolkien.png to /Users/chris/Workspace/Personal/boot.dev/bootdev_ssg/public/images/tolkien.png",
             "Copying /Users/chris/Workspace/Personal/boot.dev/bootdev_ssg/static/index.css to /Users/chris/Workspace/Personal/boot.dev/bootdev_ssg/public/index.css"
         ], self.log)
 
+    @unittest.skip("need to control which runs")
     def test_delete_files(self):
-        delete_files("./public", self.logger)
+        delete_files("./public", self.logger, True)
         self.assertListEqual([
+            "Running in test mode",
             "Removing files from /Users/chris/Workspace/Personal/boot.dev/bootdev_ssg/public",
         ], self.log)
