7e2e018610c902f0e495b1dfc56c5ee9454abb6e
Chris Tattum <chris.tattum@gmail.com>
2026-05-27 08:13:15 +0100

    Add extract_title stub and tests

---
+++ src/test_generate_pages.py
@@ -0,0 +1,13 @@
+import unittest
+from generate_pages import extract_title
+
+class TestGeneratePages(unittest.TestCase):
+    def test_extract_title_pulls_heading_1(self):
+        md = "Here is some text\n# This is my title\n\nHere is a paragraph"
+        title = extract_title(md)
+        self.assertEqual("This is my title", title)
+
+    def test_extract_title_throws_exception_if_missing(self):
+        md = "Here is some text\n# This is my title\n\nHere is a paragraph"
+        with self.assertRaises(RuntimeError):
+            title = extract_title(md)
